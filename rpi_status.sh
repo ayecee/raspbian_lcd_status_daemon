9bf3d4f686bb0bc8493d820e491b263118c32153
Andrew Orr <andrew@andreworr.ca>
2012-12-12 23:45:40 -0600

    Trimmed down and renamed rpi_status.sh

--- rpi_status.sh
+++ rpi_status.sh
@@ -4,6 +4,6 @@ temp=`vcgencmd measure_temp | cut -d'=' -f2`
 clock_arm=`vcgencmd measure_clock arm | cut -d'=' -f2`
 volts=`vcgencmd measure_volts core | cut -d'=' -f2`
 
-echo " temp: $temp"
-echo " freq: $clock_arm"
-echo "volts: $volts"
+echo "t: $temp"
+echo "f: $clock_arm"
+echo "v: $volts"
