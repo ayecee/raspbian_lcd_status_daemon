88c55600d8fd68c877f800c0fbbad2cf9dba0c78
Andrew Orr <andrew@andreworr.ca>
2012-12-15 01:04:31 -0600

    Added cpu usage to rpi_status.sh

--- rpi_status.sh
+++ rpi_status.sh
@@ -3,5 +3,10 @@
 temp=`vcgencmd measure_temp | cut -d'=' -f2`
 ip=`ifconfig eth0 | grep inet | cut -d':' -f2 | cut -d' ' -f1`
 time=`date | cut -d' ' -f4`
+idle=`top -bn2 | grep %Cpu | tail -n1 | sed 's/^.*ni, *//g' | cut -d' ' -f1`
+cpu=$(echo 100.0 - $idle | bc)
 
-echo "Time: $time              Temp: $temp                $ip"
+echo "Time: $time"
+echo "%CPU: $cpu"
+echo "Temp: $temp"
+echo "$ip"
