2843975203789263b1f657c65a6108d7e799ba42
Andrew Orr <aorr@iders.ca>
2012-12-19 16:23:38 -0600

    Updated cpu usage in rpi_status.sh, updated todo

--- rpi_status.sh
+++ rpi_status.sh
@@ -3,8 +3,7 @@
 temp=`vcgencmd measure_temp | cut -d'=' -f2`
 ip=`ifconfig eth0 | grep inet | cut -d':' -f2 | cut -d' ' -f1`
 time=`date | cut -d' ' -f4`
-idle=`top -bn2 | grep %Cpu | tail -n1 | sed 's/^.*ni, *//g' | cut -d' ' -f1`
-cpu=$(echo 100.0 - $idle | bc)
+cpu=`ps -e -o pcpu | tail -n +2 | sed '{:q;N;s/\n/ + /g;t q}' | bc`
 memtotal=`cat /proc/meminfo | head -n1 | sed 's/.*: *//g' | cut -d' ' -f1`
 memfree=`cat /proc/meminfo | head -n2 | tail -n1 | sed 's/.*: *//g' | cut -d' ' -f1`
 memused=$(echo $memtotal - $memfree | bc)
