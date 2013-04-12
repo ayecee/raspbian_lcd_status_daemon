23627d9cb73f4cfc3b8b2f483256e84de0592d18
ayecee <ayecee@gmail.com>
2013-04-12 09:34:12 -0500

    Update rpi_status.sh
    Reduce program invocations

--- rpi_status.sh
+++ rpi_status.sh
@@ -2,12 +2,12 @@
 
 temp=`vcgencmd measure_temp | cut -d'=' -f2`
 ip=`ifconfig eth0 | grep inet | cut -d':' -f2 | cut -d' ' -f1`
-time=`date | cut -d' ' -f4`
+time=`date +%T`
 cpu=`ps -e -o pcpu | tail -n +2 | sed '{:q;N;s/\n/ + /g;t q}' | bc`
-memtotal=`cat /proc/meminfo | head -n1 | sed 's/.*: *//g' | cut -d' ' -f1`
-memfree=`cat /proc/meminfo | head -n2 | tail -n1 | sed 's/.*: *//g' | cut -d' ' -f1`
-memused=$(echo $memtotal - $memfree | bc)
-mem=$(echo $memused \* 100 / $memtotal | bc)
+memtotal=`head -n1 /proc/meminfo | sed 's/.*: *//g' | cut -d' ' -f1`
+memfree=`head -n2 /proc/meminfo | tail -n1 | sed 's/.*: *//g' | cut -d' ' -f1`
+memused=$(($memtotal - $memfree))
+mem=$(($memused * 100 / $memtotal))
 
 echo "Time: $time"
 echo " Cpu: $cpu %"
